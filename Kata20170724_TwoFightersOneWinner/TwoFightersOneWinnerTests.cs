9db8948b99a191f1af86c9b589345db2768532a6
cash <cash@cashwu.com>
2017-07-24 18:15:29 +0800

    finished kata

--- Kata20170724_TwoFightersOneWinner/TwoFightersOneWinnerTests.cs
+++ Kata20170724_TwoFightersOneWinner/TwoFightersOneWinnerTests.cs
@@ -11,6 +11,36 @@ namespace Kata20170724_TwoFightersOneWinner
             AssertWinnerShoudBe(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew", "Lew");
         }
 
+        [TestMethod]
+        public void TwoFightersOneWinnerTest2()
+        {
+            AssertWinnerShoudBe(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Harry", "Harry");
+        }
+
+        [TestMethod]
+        public void TwoFightersOneWinnerTest3()
+        {
+            AssertWinnerShoudBe(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harry", "Harald");
+        }
+
+        [TestMethod]
+        public void TwoFightersOneWinnerTest4()
+        {
+            AssertWinnerShoudBe(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harald", "Harald");
+        }
+
+        [TestMethod]
+        public void TwoFightersOneWinnerTest5()
+        {
+            AssertWinnerShoudBe(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Jerry", "Harald");
+        }
+
+        [TestMethod]
+        public void TwoFightersOneWinnerTest6()
+        {
+            AssertWinnerShoudBe(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Harald", "Harald");
+        }
+
         private static void AssertWinnerShoudBe(Fighter fighter1, Fighter fighter2, string firstAttacker, string expected)
         {
             var kata = new Kata();
@@ -23,26 +53,51 @@ namespace Kata20170724_TwoFightersOneWinner
     {
         public string declareWinner(Fighter fighter1, Fighter fighter2, string firstAttacker)
         {
-            var winner = string.Empty;
+            string winner;
             do
             {
-                Fighter1AttachFighter2(fighter1, fighter2);
-                if (Fighter2HadDeath(fighter2))
+                if (fighter1.Name == firstAttacker)
                 {
-                    winner = fighter1.Name;
-                    break;
-                }
+                    winner = Fighter1AttachFighterAndCheckWinner(fighter1, fighter2);
+                    if (winner != "") break;
 
-                Fighter2AttachFighter1(fighter1, fighter2);
-                if (Fighter1HadDeath(fighter1))
+                    winner = Figher2AttachFighterAndCheckWinner(fighter1, fighter2);
+                    if (winner != "") break;
+                }
+                else
                 {
-                    winner = fighter2.Name;
-                    break;
+                    winner = Figher2AttachFighterAndCheckWinner(fighter1, fighter2);
+                    if (winner != "") break;
+
+                    winner = Fighter1AttachFighterAndCheckWinner(fighter1, fighter2);
+                    if (winner != "") break;
                 }
 
             } while (!Fighter1HadDeath(fighter1) && !Fighter2HadDeath(fighter2));
 
             return winner;
+        }
+
+        private static string Figher2AttachFighterAndCheckWinner(Fighter fighter1, Fighter fighter2)
+        {
+            Fighter2AttachFighter1(fighter1, fighter2);
+            if (Fighter1HadDeath(fighter1))
+            {
+                return fighter2.Name;
+            }
+
+            return string.Empty;
+        }
+
+        private static string Fighter1AttachFighterAndCheckWinner(Fighter fighter1, Fighter fighter2)
+        {
+            Fighter1AttachFighter2(fighter1, fighter2);
+            if (Fighter2HadDeath(fighter2))
+            {
+                return fighter1.Name;
+            }
+
+            return string.Empty;
         }
 
         private static bool Fighter1HadDeath(Fighter fighter1)
